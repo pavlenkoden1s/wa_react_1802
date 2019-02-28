21af69ecdbb02bffc38685fe1cd3bbcad5e9c454
Ilya Lytvynov <ilyaillych@gmail.com>
2019-02-28 22:11:01 +0200

    Added redux

--- unsplash-clone/src/components/nav/Nav.tsx
+++ unsplash-clone/src/components/nav/Nav.tsx
@@ -1,12 +1,42 @@
 import { SearchForm } from '../searchForm';
-import React from 'react';
+import React, { SyntheticEvent } from 'react';
+import { connect } from 'react-redux';
+import { fetchItems } from '../../actions';
 
 import './Nav.scss';
 
-export const Nav: React.FunctionComponent<any> = ({onSearch}) => {
+interface IProps {
+  textFromRedux: string;
+  onSearch: (e: any) => void,
+  onButtonClick: (s: SyntheticEvent<HTMLButtonElement>) => void
+}
+
+const Nav: React.FunctionComponent<IProps> = ({onSearch, textFromRedux, onButtonClick}) => {
   return <nav className={'page-nav'}>
     <div className='page-nav__container'>
       <SearchForm className={'page-nav__search-form'} onSubmit={onSearch}/>
+      <button onClick={onButtonClick}>{textFromRedux}</button>
     </div>
   </nav>
 };
+
+const mapStateToProps = (state: any) => {
+  console.log(state);
+  return {
+    textFromRedux: state.unsplash.text
+  }
+};
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    onButtonClick: (e: SyntheticEvent<HTMLButtonElement>) => {
+      dispatch(fetchItems())
+    }
+  }
+};
+
+const ConnectedNav = connect(mapStateToProps, mapDispatchToProps)(Nav);
+
+export {ConnectedNav as Nav};
+
+
--- unsplash-clone/src/index.tsx
+++ unsplash-clone/src/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer  from './reducers';
 
 import './index.scss';
+// @ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const store = createStore(rootReducer, composeEnhancers());
 
 import { App } from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root'));
